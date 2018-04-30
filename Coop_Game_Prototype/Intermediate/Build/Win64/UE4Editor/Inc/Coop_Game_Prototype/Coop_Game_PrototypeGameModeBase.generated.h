e6ce6d9ab4f06950a5627356b7d73210733b4412
Jer <trevino1991@gmail.com>
2018-04-30 17:33:30 -0400

    Initial Commit - Project Setup

---
+++ Coop_Game_Prototype/Intermediate/Build/Win64/UE4Editor/Inc/Coop_Game_Prototype/Coop_Game_PrototypeGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef COOP_GAME_PROTOTYPE_Coop_Game_PrototypeGameModeBase_generated_h
+#error "Coop_Game_PrototypeGameModeBase.generated.h already included, missing '#pragma once' in Coop_Game_PrototypeGameModeBase.h"
+#endif
+#define COOP_GAME_PROTOTYPE_Coop_Game_PrototypeGameModeBase_generated_h
+
+#define Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_RPC_WRAPPERS
+#define Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACoop_Game_PrototypeGameModeBase(); \
+	friend COOP_GAME_PROTOTYPE_API class UClass* Z_Construct_UClass_ACoop_Game_PrototypeGameModeBase(); \
+public: \
+	DECLARE_CLASS(ACoop_Game_PrototypeGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/Coop_Game_Prototype"), NO_API) \
+	DECLARE_SERIALIZER(ACoop_Game_PrototypeGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACoop_Game_PrototypeGameModeBase(); \
+	friend COOP_GAME_PROTOTYPE_API class UClass* Z_Construct_UClass_ACoop_Game_PrototypeGameModeBase(); \
+public: \
+	DECLARE_CLASS(ACoop_Game_PrototypeGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/Coop_Game_Prototype"), NO_API) \
+	DECLARE_SERIALIZER(ACoop_Game_PrototypeGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACoop_Game_PrototypeGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACoop_Game_PrototypeGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACoop_Game_PrototypeGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACoop_Game_PrototypeGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACoop_Game_PrototypeGameModeBase(ACoop_Game_PrototypeGameModeBase&&); \
+	NO_API ACoop_Game_PrototypeGameModeBase(const ACoop_Game_PrototypeGameModeBase&); \
+public:
+
+
+#define Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACoop_Game_PrototypeGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACoop_Game_PrototypeGameModeBase(ACoop_Game_PrototypeGameModeBase&&); \
+	NO_API ACoop_Game_PrototypeGameModeBase(const ACoop_Game_PrototypeGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACoop_Game_PrototypeGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACoop_Game_PrototypeGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACoop_Game_PrototypeGameModeBase)
+
+
+#define Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_12_PROLOG
+#define Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_RPC_WRAPPERS \
+	Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_INCLASS \
+	Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Coop_Game_Prototype_Source_Coop_Game_Prototype_Coop_Game_PrototypeGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Coop_Game_Prototype/Source/Coop_Game_Prototype/Coop_Game_PrototypeGameModeBase.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Coop_Game_PrototypeGameModeBase.h"
+
+
+
+
